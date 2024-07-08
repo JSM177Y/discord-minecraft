7189ddc64777733ddef5ca39107826298c2d5198
= <jsmittee7@gmail.com>
2024-07-07 21:03:36 -0400

    intents

--- bot.py
+++ bot.py
@@ -13,7 +13,10 @@ RCON_HOST = os.getenv('RCON_HOST')
 RCON_PORT = int(os.getenv('RCON_PORT'))
 RCON_PASSWORD = os.getenv('RCON_PASSWORD')
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
